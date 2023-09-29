5201cbd6c5aa26cfe07ead8be4f51ffda097831e
Aditya Kumar <ak9001099436@gmail.com>
2023-09-29 16:38:11 +0530

    Version 2.0
    This the new version of Project Blocker which is being built using Compose Desktop

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+}
+
+group = "com.blocker"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    val lifecycle_version = "2.6.2"
+    // Note, if you develop a library, you should use compose.desktop.common.
+    // compose.desktop.currentOs should be used in launcher-sourceSet
+    // (in a separate module for demo project and in testMain).
+    // With compose.desktop.common you will also lose @Preview functionality
+
+    api("dev.icerock.moko:mvvm-core:0.16.1") // only ViewModel, EventsDispatcher, Dispatchers.UI
+    api("dev.icerock.moko:mvvm-compose:0.16.1")
+    implementation(compose.desktop.currentOs)
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Blocker"
+            packageVersion = "1.0.0"
+        }
+    }
+}
---
+++ src/main/kotlin/data/main/mainState.kt
@@ -0,0 +1,11 @@
+package data.main
+
+data class mainState (
+    val search: String="",
+    val home: Boolean= true,
+    val blocker:Boolean=false,
+    val monitor: Boolean=false,
+    val restrict: Boolean= false,
+    val summary: Boolean=false,
+    val settings: Boolean=false
+)
